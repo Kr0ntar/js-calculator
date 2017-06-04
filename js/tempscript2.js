03c205df71a5133017c7cb9dc5ed90a0d50f38c5
Kr0ntar <jleorico@gmail.com>
2017-06-04 16:37:28 +0400

    finished refactoring js code

--- js/tempscript2.js
+++ js/tempscript2.js
@@ -15,6 +15,13 @@ var eightBtn = document.querySelector('#eight-button');
 var nineBtn = document.querySelector('#nine-button');
 var zeroBtn = document.querySelector('#zero-button');
 var pointBtn = document.querySelector('#point-button');
+var plusBtn = document.querySelector('#plus-button');
+var minusBtn = document.querySelector('#minus-button');
+var timesBtn = document.querySelector('#times-button');
+var divideBtn = document.querySelector('#divide-button');
+var equalBtn = document.querySelector('#equal-button');
+var clearBtn = document.querySelector('#clear-button');
+var clrEntBtn = document.querySelector('#clear-entry-button');
 
 
 /* getting input from the buttons */
@@ -27,6 +34,7 @@ sixBtn.addEventListener('click', function() { inputHandler('6'); });
 sevenBtn.addEventListener('click', function() { inputHandler('7'); });
 eightBtn.addEventListener('click', function() { inputHandler('8'); });
 nineBtn.addEventListener('click', function() { inputHandler('9'); });
+
 zeroBtn.addEventListener('click', function() {
 	tempnum = "";
 	if(output.value !== "0") {
@@ -53,207 +61,66 @@ pointBtn.addEventListener('click', function() {
 	}
 });
 
+
+/* operations & functions */
+plusBtn.addEventListener('click', function() { opsHandler('+'); });
+minusBtn.addEventListener('click', function() { opsHandler('-'); });
+timesBtn.addEventListener('click', function() { opsHandler('*'); });
+divideBtn.addEventListener('click', function() { opsHandler('/'); });
+
+equalBtn.addEventListener('click', function() {
+	expression.push(parseFloat(output.value));
+	var strEval = expression.join('');
+	var answer = eval(strEval);
+	tempnum = answer;
+	output.value = answer;
+	ops_output.value = output.value;
+	expression = [];
+	num = "";
+});
+
+clearBtn.addEventListener('click', function() {
+	num = "", tempnum = "", output.value = "0", ops_output.value = "", expression = [];
+});
+
+clrEntBtn.addEventListener('click', function() {
+	if(expression[expression.length - 1] == '+') {
+		tempnum = "";
+		output.value = '0';
+		ops_output.value = expression.join('');
+	} else {
+		tempnum = "";
+		expression.pop();
+		output.value = '0';
+		ops_output.value = expression.join('');
+	}
+});
+
 function inputHandler(num_input) {
 	tempnum = "";
 	num += Number(num_input);
 	output.value = num;	
 }
 
-// function one() {
-// 	tempnum = "";
-// 	num += Number('1');
-// 	output.value = num;
-// }
+function opsHandler(ops_input) {
+	if(tempnum !== "") {
+		expression.push(tempnum);
+		expression.push(ops_input);
+		tempnum = "";
+	}
 
-// function two() {
-// 	tempnum = "";
-// 	num += Number('2');
-// 	output.value = num;
-// }
-
-// function three() {
-// 	tempnum = "";
-// 	num += Number('3');
-// 	output.value = num;
-// }
-
-// function four() {
-// 	tempnum = "";
-// 	num += Number('4');
-// 	output.value = num;
-// }
-
-// function five() {
-// 	tempnum = "";
-// 	num += Number('5');
-// 	output.value = num;
-// }
-
-// function six() {
-// 	tempnum = "";
-// 	num += Number('6');
-// 	output.value = num;
-// }
-
-// function seven() {
-// 	tempnum = "";
-// 	num += Number('7');
-// 	output.value = num;
-// }
-
-// function eight() {
-// 	tempnum = "";
-// 	num += Number('8');
-// 	output.value = num;
-// }
-
-// function nine() {
-// 	tempnum = "";
-// 	num += Number('9');
-// 	output.value = num;
-// }
-
-// function zero() {
-// 	tempnum = "";
-// 	if(output.value !== "0") {
-// 		num += Number('0');		
-// 		output.value = num;
-// 	} else {
-// 		num = num;
-// 	}
-// }
-
-// function point() {
-// 	var tempStr = output.value;
-// 	var tempArr = tempStr.split('');
-// 	var val = '.';
-
-// 	if(tempArr.indexOf(val) == -1 && tempStr != "0") {
-// 		num += '.';
-// 		output.value = num;
-// 	} else if(tempArr.indexOf(val) == -1 && tempStr == "0") {
-// 		num += "0.";
-// 		output.value = num;
-// 	} else {
-// 		num = num;
-// 	}
-// }
-
-
-/* operations */
-function clr() { 
-	num = "", output.value = "0", ops_output.value = "", expression = [];
-}
-
-function clrEnt() {
-	if(expression[expression.length - 1] == '+') {
-		output.value = '0';
+	if(expression.length == 0 && num == "") {
+		expression.push('0');
+		expression.push(ops_input);
 		ops_output.value = expression.join('');
-	} else {
-		expression.pop();
-		output.value = '0';
-		ops_output.value = expression.join('');
-	}
-}
-
-function plus() {
-	console.log(expression);
-	console.log(tempnum);
-	if(tempnum !== "") {
-		expression.push(tempnum);
-		expression.push('+');
-		tempnum = "";
-	}
-
-	if(expression.length == 0 && num == "") {
-		return false;
+		//return;
 	}
 
 	if(expression.length >= 0 && num !== "") {
 		expression.push(parseFloat(num));
-		expression.push('+');
-		console.log(expression);
+		expression.push(ops_input);
 	}
+
 	num = "";
 	ops_output.value = expression.join('');
 }
-
-function minus() {
-	console.log(expression);
-	console.log(tempnum);
-	if(tempnum !== "") {
-		expression.push(tempnum);
-		expression.push('-');
-		tempnum = "";
-	}
-
-	if(expression.length == 0 && num == "") {
-		return false;
-	}
-
-	if(expression.length >= 0 && num !== "") {
-		expression.push(parseFloat(num));
-		expression.push('-');
-		console.log(expression);
-	}
-	num = "";
-	ops_output.value = expression.join('');
-}
-
-function times() {
-	console.log(expression);
-	console.log(tempnum);
-	if(tempnum !== "") {
-		expression.push(tempnum);
-		expression.push('*');
-		tempnum = "";
-	}
-
-	if(expression.length == 0 && num == "") {
-		return false;
-	}
-
-	if(expression.length >= 0 && num !== "") {
-		expression.push(parseFloat(num));
-		expression.push('*');
-		console.log(expression);
-	}
-	num = "";
-	ops_output.value = expression.join('');
-}
-
-function divide() {
-	console.log(expression);
-	console.log(tempnum);
-	if(tempnum !== "") {
-		expression.push(tempnum);
-		expression.push('/');
-		tempnum = "";
-	}
-
-	if(expression.length == 0 && num == "") {
-		return false;
-	}
-
-	if(expression.length >= 0 && num !== "") {
-		expression.push(parseFloat(num));
-		expression.push('/');
-		console.log(expression);
-	}
-	num = "";
-	ops_output.value = expression.join('');
-}
-
-function ans() {
-	expression.push(parseFloat(output.value));
-	var strEval = expression.join('');
-	var answer = eval(strEval);
-	console.log(expression);
-	tempnum = answer;
-	output.value = answer;
-	ops_output.value = output.value;
-	expression = [];
-	num = "";
-	console.log(tempnum);
-}
-
